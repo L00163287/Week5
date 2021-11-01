2cb4813a96f025b2467af5340cf42d3ddd968d2b
L00163287 <l00163287@student.lyit.ie>
2021-11-01 09:45:30 +0000

    "Initial commit for file handling"

---
+++ Questions/Question01.py
@@ -0,0 +1,11 @@
+"""
+#
+# @File         : Question01.py
+# @Created      : 2021-10-19 09:44
+# @Author       : Bubashankushan B
+# @Version      : v1.0.0
+# @Licensing    : 
+#
+# @Description  :
+#
+"""
